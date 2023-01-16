bd3632ce628e7e40d93b6b2812b45afd30ebbe0a
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-01-16 21:44:29 +0700

    feat:added sass instead of less

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import './App.scss'
+
 function App() {
-  return <div className='App'>hello</div>
+  return (
+    <div className='App'>
+      <div className='App-1'>hello</div>
+    </div>
+  )
 }
 
 export default App
