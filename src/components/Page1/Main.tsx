d0ac3d36ac18afce75d873831db6ae871c7f2b78
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-01-20 13:43:57 +0700

    fix: lil style fix

--- src/components/Page1/Main.tsx
+++ src/components/Page1/Main.tsx
@@ -13,21 +13,21 @@ const blockchainData = ['Solidity', 'ERC20', 'ERC741', 'ERC1155', 'EthersJS', 'D
 const commonData = ['Git', 'Eslint', 'Prettier']
 const contentItemData = [
   {
-    text: '24 years old energetic, hard-working programmer. I love travelling, doing sports, watching movies. Tookpart in the work and travel program.',
+    text: '24 years old energetic, hard-working programmer. I love travelling, doing sports, watching movies. Took  part in the work and travel program.',
     imgHeight: 270,
     imgWidth: 200,
     textFirst: true,
     src: [good, bad],
   },
   {
-    text: 'Education - Kazan Federal University, institute of economic and financial management, specialist ofeconomical security, years: 2016-2021, also - certificate udemy react and certificate udemy node JS',
+    text: 'Education - Kazan Federal University, institute of economic and financial management, specialist of economical security, years: 2016-2021, also - certificate udemy react and certificate udemy node JS',
     imgHeight: 300,
     imgWidth: 200,
     textFirst: false,
     src: [education],
   },
   {
-    text: 'Experience - 2+ year commercial development, over 10 commercial projects including development of pharmacyapps, functional implementation based on gnosis chain, develop new user-facing features, build reusablecode and libraries for future use, optimize application for maximum speed and scalability',
+    text: 'Experience - 2+ year commercial development, over 10 commercial projects including development of pharmacy apps, functional implementation based on gnosis chain, develop new user-facing features, build reusable code and libraries for future use, optimize application for maximum speed and scalability',
     imgHeight: 220,
     imgWidth: 300,
     textFirst: true,
@@ -44,8 +44,9 @@ const Main = () => {
         <div className='main_welcome_text'> Hi my name is Danil and I'm a frontend developer</div>
       </div>
       <div className='main_content'>
-        {contentItemData.map((item) => (
+        {contentItemData.map((item, key) => (
           <ContentItem
+            key={key}
             src={item.src}
             text={item.text}
             imgWidth={item.imgWidth}
