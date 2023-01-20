35460908b4fbc367e0b6c18f0c11d07f5a2d6ebc
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-01-20 12:46:07 +0700

    feat: added framer-motion pack

--- src/components/Page1/ContentItem/ContentItem.tsx
+++ src/components/Page1/ContentItem/ContentItem.tsx
@@ -1,28 +1,37 @@
 import { useContext } from 'react'
 import ComboContext from '../../../Context/ComboContext'
 import { IContentItem } from '../../../Types/types'
+import { motion } from 'framer-motion'
 
 const ContentItem = ({ text, imgWidth, imgHeight, textFirst, src }: IContentItem) => {
   const { theme } = useContext(ComboContext)
+
+  const MotionDiv = () => (
+    <motion.div
+      initial={{ y: 200 }}
+      whileInView={{ y: 0 }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+      className='main_content_item_img'
+    >
+      <img
+        src={src.length > 1 ? (theme === 'light' ? src[0] : src[1]) : src[0]}
+        alt='fireSpot'
+        style={{ width: imgWidth, height: imgHeight }}
+      />
+    </motion.div>
+  )
 
   return textFirst ? (
     <div className='main_content_item'>
       <div className='main_content_item_text'>
         <div className='main_content_item_text_content'>{text}</div>
       </div>
-      <div className='main_content_item_img'>
-        <img
-          src={src.length > 1 ? (theme === 'light' ? src[0] : src[1]) : src[0]}
-          alt='fireSpot'
-          style={{ width: imgWidth, height: imgHeight }}
-        />
-      </div>
+      <MotionDiv />
     </div>
   ) : (
     <div className='main_content_item'>
-      <div className='main_content_item_img'>
-        <img src={src[0]} alt='fireSpot' style={{ width: imgWidth, height: imgHeight }} />
-      </div>
+      <MotionDiv />
       <div className='main_content_item_text'>
         <div className='main_content_item_text_content'>{text}</div>
       </div>
