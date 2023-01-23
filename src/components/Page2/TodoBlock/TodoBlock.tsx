98185c1079d1e00b30563d6cfa50d9069a507079
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-01-23 15:11:26 +0700

    feat: diff scss

--- src/components/Page2/TodoBlock/TodoBlock.tsx
+++ src/components/Page2/TodoBlock/TodoBlock.tsx
@@ -3,13 +3,13 @@ import Todo from './Todo/Todo'
 
 const TodoBlock = () => {
   return (
-    <div className='todos_content_first'>
-      <div className='todos_content_first_info'>
-        <div className='todos_content_first_info_text'>
+    <div className='block'>
+      <div className='block_info'>
+        <div className='block_info_text'>
           Let's test an ordinary todo list. There is nothing special, state management is in base react state.{' '}
         </div>
-        <div className='todos_content_first_info_img'>
-          <img src={circle} alt='alt' className='todos_content_first_info_img_content' />
+        <div className='block_info_img'>
+          <img src={circle} alt='alt' className='block_info_img_content' />
         </div>
       </div>
       <Todo />
