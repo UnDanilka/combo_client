099b0c5e4de18c2382879415699cf9c1c2669deb
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-01-25 11:08:06 +0700

    feat: motion for the drawer

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,10 +1,35 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { useContext } from 'react'
 import ComboContext from '../../Context/ComboContext'
 
-const Drawer = () => {
-  const { isDrawer } = useContext(ComboContext)
+const menuMotion = {
+  initial: { x: '-100%' },
+  animate: { x: 0 },
+  exit: { x: '-100%' },
+  transition: { duration: 0.3 },
+}
+const layoutMotion = {
+  exit: { opacity: 0 },
+  transition: { duration: 0.3 },
+}
 
-  return isDrawer ? <div className='drawer'></div> : <div></div>
+const Drawer = () => {
+  const { isDrawer, handleUpdateIsDrawer } = useContext(ComboContext)
+
+  return (
+    <AnimatePresence>
+      {isDrawer ? (
+        <motion.div key='layout' {...layoutMotion} className='drawer'>
+          <motion.div key='menu' {...menuMotion} className='drawer_menu'>
+            <div className='drawer_menu_header'></div>
+          </motion.div>
+          <div className='drawer_space' onClick={() => handleUpdateIsDrawer(false)}></div>
+        </motion.div>
+      ) : (
+        <div></div>
+      )}
+    </AnimatePresence>
+  )
 }
 
 export default Drawer
