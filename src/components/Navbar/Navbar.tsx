d9186d0a2c53976a8ee5fd112451f55ae2831cc0
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2023-01-26 15:10:57 +0700

    fix: fixed theme issue

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import ComboContext from '../../Context/ComboContext'
 import NavbarSmall from './NavbarSmall/NavbarSmall'
@@ -6,28 +6,48 @@ import Theme from './Theme/Theme'
 
 const Navbar = () => {
   const { theme, links } = useContext(ComboContext)
+  const [size, setSize] = useState('big')
+
+  useEffect(() => {
+    const handleWidthChecker = (e: UIEvent) => {
+      const window = e.target as Window
+      if (window.innerWidth > 700) {
+        setSize('big')
+      } else {
+        setSize('small')
+      }
+    }
+
+    window.addEventListener('resize', (e) => handleWidthChecker(e))
+    return () => {
+      window.removeEventListener('resize', handleWidthChecker)
+    }
+  }, [])
 
   return (
     <>
-      <div className='navbar'>
-        <div className='navbar_items'>
-          {links.map((link) => {
-            return (
-              <div className='navbar_items_item '>
-                <Link
-                  className={`navbar_items_item_link ${theme === 'light' ? 'navbar_light' : 'navbar_dark'}`}
-                  to={link.link}
-                >
-                  {link.title}
-                </Link>
-              </div>
-            )
-          })}
+      {size === 'big' ? (
+        <div className='navbar'>
+          <div className='navbar_items'>
+            {links.map((link) => {
+              return (
+                <div className='navbar_items_item '>
+                  <Link
+                    className={`navbar_items_item_link ${theme === 'light' ? 'navbar_light' : 'navbar_dark'}`}
+                    to={link.link}
+                  >
+                    {link.title}
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+          <Theme />
+          <div className='navbar_divider' />
         </div>
-        <Theme />
-        <div className='navbar_divider' />
-      </div>
-      <NavbarSmall />
+      ) : (
+        <NavbarSmall />
+      )}
     </>
   )
 }
